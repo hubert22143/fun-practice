78b1428a2f2a50edc2c96962b6264e06d3d70f6f
Hubert Baciak <hubert.baciak@o2.pl>
2023-10-03 17:25:13 +0200

    c++ exercise

---
+++ first steps in c++/...RandomGuessNumber/randomGuessNumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib> 
+
+int main(){
+    int num;
+    int tries;
+    int guess;
+    tries = 0;
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+    std::cout<< " Random Guessing Game ! " << '\n';
+    std::cout << " Write your guessing number" << '\n';
+    do{
+        std::cin>>guess;
+        tries++;
+        std::cout<< " Your current tries in guessing the number:" << tries << '\n';
+    } while (num != guess); 
+    std::cout << "Congratulations! You've won!!" << '\n';
+    
